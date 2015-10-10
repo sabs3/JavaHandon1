c107b92c6e3bc6cd52b7aa0cf39d099a77ed4439
sabs3 <sabinakhanal@hotmail.co.uk>
2015-10-10 16:32:12 +0100

    new update

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		int subject;
+		int i = 0;
+		int average = 0;
+		
+		Scanner user_input = new Scanner(System.in);
+		
+		System.out.println("Enter the name of student");
+		String student = user_input.next();
+		
+		System.out.print("Enter the number of Subjects: ");
+		int numberofSub = user_input.nextInt();
+		
+		int arrayofMarks[] = new int[numberofSub];		
+		
+		
+		for (i = 0; i< numberofSub; i++) {
+			System.out.print("Enter the mark for subject " + (i+1) + " : ");
+			subject = user_input.nextInt();
+			StudentMark grade = new StudentMark(subject);
+			arrayofMarks[i] = subject;
+			System.out.println(student + " got " + grade.getComment() + " grade for subject " + (i+1));
+			}
+		for (i =0; i<numberofSub; i++) {
+			average =average+arrayofMarks[i];
+			}
+		average = average/100;
+		StudentMark avegrade = new StudentMark(average);
+		System.out.println("The average is" + " : " + avegrade.getComment());
+	}	
+
+}
--- src/StudentMark.java
+++ src/StudentMark.java
@@ -1,42 +1,37 @@
 import java.util.Scanner;
 public class StudentMark {
 	
-public static void main(String []args){
-		
-		Scanner user_input = new Scanner(System.in);
-		
-		String student;
-		System.out.print("Enter the student name: ");
-		student= user_input.next();
-		
-		int math, english, hindi, science, social;
-
-		System.out.print("Enter student mark for Mathematics, English, Hindi, Science & Social Science: ");
-		math = user_input.nextInt();
-		english = user_input.nextInt();
-		hindi = user_input.nextInt();
-		science = user_input.nextInt();
-		social = user_input.nextInt();
-		
-		int [] mark = {math, english, hindi, science, social};
-		String[] subjects = new String[] {"Mathematics", "English", "Hindi", "Science", "Social Science"};
-		
-		String comment = null;
-		for (int x=0; x<(mark.length); x++){
-		if(mark[x] <40){
+	private int mark;
+	private String comment;
+	
+	public StudentMark(int mark){
+		this.mark = mark;
+		grades();
+	}
+	
+	public void grades() {
+		if(mark < 40) {
 			comment = "Poor";
-		}else if (mark[x] <60){
+		}else if (mark < 60) {
 			comment = "Average";
-		}else if (mark[x] < 80){
+		}else if (mark < 80) {
 			comment = "Good";
-		}else if (mark[x] < 90){
-			 comment = "Very Good";
-		}else if (mark[x] >= 90){
+		}else if (mark < 90) {
+			comment = "Very Good";
+		}else if (mark >= 90) {
 			comment = "Excellent";
-			}
-		System.out.println(student + " got " + comment + " grade in "+ subjects[x]);
 		}
 	}
-}
+	
+	public String getComment() {
+		return comment;		
+	}
+	
+	
+	}
+	
+	
+
+
 
 
